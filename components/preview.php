9e8465e431b68d5bd28ffc870d110691936c7a0a
Benni Gras <benjamin.gras@outlook.de>
2021-06-24 08:19:14 +0200

    Adding link to the icon

--- components/preview.php
+++ components/preview.php
@@ -52,7 +52,7 @@
             , 
             <?php echo e($user->city); ?></span>  
 
-            <i class="far fa-heart"></i>
+            <a href="change_merkliste?id=<?php echo e($item->id); ?>"><i class="far fa-heart"></i></a>
             
         </p>
         
